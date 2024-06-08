70769a8e165451b910386453b192ca677d8e60be
marco <119810706+array-marco@users.noreply.github.com>
2024-06-08 23:20:38 +0300

    payment method listview

--- lib/Features/checkout/presentation/views/payment_details_body.dart
+++ lib/Features/checkout/presentation/views/payment_details_body.dart
@@ -10,15 +10,34 @@ class PaymentDetailsBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CustomPaymentMethod(image: 'assets/images/card.svg'),
-              CustomPaymentMethod(image: 'assets/images/paypal.svg'),
-              CustomPaymentMethod(image: 'assets/images/apple_pay.svg'),
-            ],
-          )
+          PaymentMethodsListView(),
         ],
+      ),
+    );
+  }
+}
+
+class PaymentMethodsListView extends StatelessWidget {
+  const PaymentMethodsListView({super.key});
+  final List<String> paymentMethodsItems = const [
+    'assets/images/card.svg',
+    'assets/images/paypal.svg',
+    'assets/images/apple_pay.svg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 65,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: paymentMethodsItems.length,
+        itemBuilder: (context, index) {
+          return CustomPaymentMethod(
+            isActive: false,
+            image: paymentMethodsItems[index],
+          );
+        },
       ),
     );
   }
@@ -28,21 +47,28 @@ class CustomPaymentMethod extends StatelessWidget {
   const CustomPaymentMethod({
     super.key,
     required this.image,
+    this.isActive = false,
   });
   final String image;
+  final bool isActive;
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 600),
       width: 110,
       height: 65,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(15),
-        border: Border.all(width: 2, color: Colors.green),
+        border:
+            Border.all(width: 2, color: isActive ? Colors.green : Colors.grey),
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SvgPicture.asset(image),
+        child: SvgPicture.asset(
+          image,
+          fit: BoxFit.scaleDown,
+        ),
       ),
     );
   }
