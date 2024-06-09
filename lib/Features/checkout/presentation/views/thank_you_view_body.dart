c5d95a933683f467b46fb8acf9c8dbf9402d2f6d
marco <119810706+array-marco@users.noreply.github.com>
2024-06-09 17:28:19 +0300

    finish custom shapes container

--- lib/Features/checkout/presentation/views/thank_you_view_body.dart
+++ lib/Features/checkout/presentation/views/thank_you_view_body.dart
@@ -6,12 +6,13 @@ class ThankYouViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
       child: Stack(
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: ShapeDecoration(
-              color: Colors.grey[400],
+              color: Colors.grey[300],
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20),
               ),
@@ -30,7 +31,30 @@ class ThankYouViewBody extends StatelessWidget {
             child: const CircleAvatar(
               backgroundColor: Colors.white,
             ),
-          )
+          ),
+          Positioned(
+            right: 0,
+            left: 0,
+            top: -45,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.grey[300],
+            ),
+          ),
+          const Positioned(
+            right: 0,
+            left: 0,
+            top: -35,
+            child: CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.green,
+              child: Icon(
+                size: 70,
+                Icons.check_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
     );
