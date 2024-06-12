ba34c5fab5b36ef88c9ed7563e2af275b3720496
marco <119810706+array-marco@users.noreply.github.com>
2024-06-12 22:01:00 +0300

    implement init payment sheet

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -2,6 +2,7 @@ import 'package:checkout/Features/checkout/data/models/payment_intent_model/paym
 import 'package:checkout/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 import 'package:checkout/core/utils/api_keys.dart';
 import 'package:checkout/core/utils/api_service.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeService {
   final ApiService apiService = ApiService();
@@ -13,5 +14,14 @@ class StripeService {
         token: ApiKeys.secretKey);
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String paymentIntentClientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntentClientSecret,
+        merchantDisplayName: 'marco',
+      ),
+    );
   }
 }
