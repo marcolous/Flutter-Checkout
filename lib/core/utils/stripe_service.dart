de3adee5c8988d3ea3fc71581b3283a46d5a458a
marco <119810706+array-marco@users.noreply.github.com>
2024-06-12 20:13:54 +0300

    create api service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,7 @@
-class StripeService{
+import 'package:checkout/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
+class StripeService{
+  PaymentIntentModel createPaymentIntent(PaymentIntentModel paymentIntentModel){
+    
+  }
 }
